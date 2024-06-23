00f23687f1c6d807eecf3b409878eea02c08e3b7
Taylor Martin <taylormartin@users.noreply.github.com>
2024-06-23 13:06:23 -0400

    data on the screen

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const BASE_URL = "http://localhost:3001"
+
+export const GET_COMMENTS_URL = `${BASE_URL}/getComments`;
---
+++ src/hook.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { GET_COMMENTS_URL } from './constants';
+
+const useComments = () => {
+  const [comments, setComments] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // debugger;
+        const response = await fetch(GET_COMMENTS_URL);
+        // if (!response.ok) {
+        //   throw new Error('Network response was not ok');
+        // }
+        const result = await response.json();
+        setComments(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { comments, loading, error };
+};
+
+export default useComments;
